8aaf884fed8a76ae3fb6bc043806f0259c7bf448
Zerthick <zerthick@gmail.com>
2018-01-03 14:32:02 -0500

    Update copyright

--- src/main/java/io/github/zerthick/mailboxes/mailbox/data/mailitem/MailItemKeys.java
+++ src/main/java/io/github/zerthick/mailboxes/mailbox/data/mailitem/MailItemKeys.java
@@ -1,20 +1,20 @@
 /*
- * Copyright (C) 2017  Zerthick
+ * Copyright (C) 2018  Zerthick
  *
- * This file is part of MailBoxes.
+ * This file is part of Mailboxes.
  *
- * MailBoxes is free software: you can redistribute it and/or modify
+ * Mailboxes is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
  *
- * MailBoxes is distributed in the hope that it will be useful,
+ * Mailboxes is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with MailBoxes.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Mailboxes.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package io.github.zerthick.mailboxes.mailbox.data.mailitem;
